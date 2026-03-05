2217baa4e8942d3d0bcbe09c069cf567e895acd6
suphileericka-dotcom <suphileericka@gmail.com>
2026-03-05 11:07:08 +0100

    fix: remove duplicate index.ts entry point

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { app } from "./app";
-
-const PORT = process.env.PORT || 8000;
-
-if (process.env.NODE_ENV !== "production") {
-  app.listen(PORT, () => {
-    console.log(`Server running on ${PORT}`);
-  });
-}
